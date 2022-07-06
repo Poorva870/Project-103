164f4398fa773293bf3cb870f95df219c1fef410
Poorva870 <80689366+Poorva870@users.noreply.github.com>
2022-07-06 16:27:09 +0530

    Update scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -2,7 +2,5 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv("data.csv")
-fig = px.scatter(df, x="date", y="cases",
-	          size="cases",color="country",
-                   size_max=60)
+fig = px.scatter(df, x="date", y="cases",color="country")
 fig.show()
